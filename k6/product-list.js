6cccc3f737a794c06c42c17d3b72bd607885e198
김량호 <rh.kim@okestro.com>
2025-08-15 17:51:29 +0900

    docs: AS-IS / TO-BE 성능 개선 report

--- k6/product-list.js
+++ k6/product-list.js
@@ -6,16 +6,13 @@ const BASE_URL = 'http://localhost:8080';
 
 export const options = {
     stages: [
-        { duration: '1m', target: 30},
-        { duration: '2m', target: 50 },
-        { duration: '3m', target: 100 },
-        { duration: '1m', target: 0 },
+        {duration: '10m', target: 3000},
     ],
 }
 
 export default function () {
 
-    http.get(`${BASE_URL}/api/v1/products?brandId=95&productSortType=LIKES_DESC&page=0&size=15`);
+    http.get(`${BASE_URL}/api/v1/products?brandId=95&productSortType=LIKES_DESC&page=0&size=10`);
 
     sleep(1)
 }
